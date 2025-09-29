1dba4ed675a9c5351284df2fcc6a6b43d9a62e2a
garva2428 <21gahuja.acc@lwetb.ie>
2025-09-29 23:11:35 +0100

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,13 @@
+number=input('Enter a number ')
+
+total=0
+for i in number:
+    total=total+1
+    
+print(total)
+    
+    
+
+
+
+    
---
+++ 20.py
@@ -0,0 +1,4 @@
+number=int(input('Non-negative number: '))
+
+for i in range(number+1,1):
+    
