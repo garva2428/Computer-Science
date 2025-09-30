aaf086f125262187f312366b0fabe1d444f02f06
garva2428 <21gahuja.acc@lwetb.ie>
2025-09-30 14:31:49 +0100

    Update 13.py

--- assignment4/13.py
+++ assignment4/13.py
@@ -1,6 +1,10 @@
+#Author Garva Ahuja
+#Date 10/9/25
+#Programme Print the sum of all numbers from 1 to a given number
 number=int(input('Number: '))
 total=0
 
 for i in range(1,number+1):
     total=total+i
     print(total)
+
