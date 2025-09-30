d67be61fed57f8356411d5d64c99b5a4bacd245e
garva2428 <21gahuja.acc@lwetb.ie>
2025-09-30 14:32:33 +0100

    Update 14.py

--- assignment4/14.py
+++ assignment4/14.py
@@ -1,3 +1,6 @@
+#Author Garva Ahuja
+#Date 10/9/25
+#Programme Print the sum of all the odd numbers from 1 to a given number
 number=int(input('Number: '))
 total=0
 
@@ -7,3 +10,4 @@ for i in range(1,number+1):
         print(total)
     else:
         pass
+
