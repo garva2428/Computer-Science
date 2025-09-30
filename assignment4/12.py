939914f73f3afcdf80fd8fb0e82a8c04584825bb
garva2428 <21gahuja.acc@lwetb.ie>
2025-09-30 14:30:17 +0100

    Update 12.py

--- assignment4/12.py
+++ assignment4/12.py
@@ -1,5 +1,10 @@
+#Author Garva Ahuja
+#Date 10/9/25
+#Programme  Print all the even numbers within the given range
+
 for i in range(1,11):
     if i%2==0:
         print(i)
     else:
         pass
+
