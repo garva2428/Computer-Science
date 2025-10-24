d6a2c75b4f097966e55b1a8e5b09d8785cecc441
garva2428 <21gahuja.acc@lwetb.ie>
2025-10-24 09:11:19 +0100

    Add files via upload

---
+++ a11q1.py
@@ -0,0 +1,38 @@
+#Date 23/10/25
+#Author Garva Ahuja
+#Programme A for loop
+#• A while loop
+#• Slicing
+
+'for loop'
+string=input('Enter a string')
+x=-1
+output=''
+for i in string:
+    output+=string[x]
+    x-=1
+    
+print(output)
+
+'while loop'
+string2=input('Enter a string: ')
+index=-1
+hello=''
+while index/-1<=len(string2):
+    n=string2[index]
+    index-=1
+    hello+=n
+    
+print(hello)
+
+'slicing'
+string3=input('Enter a string: ')
+print(string3[::-1])
+    
+
+    
+    
+    
+    
+
+    
