ec6aec4defac1fa342220fa0946ed82d404536e3
garva2428 <21gahuja.acc@lwetb.ie>
2026-09-03 09:56:40 +0100

    Refactor linear search to use a function
    Refactor linear search implementation into a function and fix logic errors.

--- linear search.py
+++ linear search.py
@@ -2,17 +2,20 @@
 #linear search
 #2/9/26
 
-list1=eval(input('Enter a list: '))
-x=int(input('Enter an element: '))
-count=0
-for i in list1:
-    count+=1
-    if i==x:
-        print(list1.index(x))
-        break
-    if count==len(list1):
-        print('-1')
+def linearsearch(a,b):
+    count=0
+    for i in a:
+        count+=1
+        if b in a:
+            return a.index(b)
+            break
+    if count==len(a)
+    return -1
 
+a=eval(input('Enter a list: '))
+b=input('Enter an element')
+hello= linearsearch(a,b)
+print(hello)
         
         
     
